24b6ac74fb972f32dbd4a9094f442b5025ec8e0c
Sajo-riyo <106347006+Sajo-riyo@users.noreply.github.com>
2024-01-24 18:46:39 +0100

    I put the bmi calculator in a function.

--- BMI Calculator.py
+++ BMI Calculator.py
@@ -1,21 +1,24 @@
 # This is a body mass index calculator
-
 # height and weight are the required inputs
+# The body mass index is the result from the division of weight by height squared
+
+def bmi_calc(hei, wei):
+
+    bmi = round(wei / (hei ** 2))
+
+    if bmi < 18.5:
+        return f" Your bmi  is {bmi},you are under weight"
+    elif 18.5 < bmi < 25:
+        return f" Your bmi  is {bmi},your weight is Normal"
+    elif 25 < bmi < 30:
+        return f" Your bmi  is {bmi},you are Overweight"
+    elif 30 < bmi < 35:
+        return f" Your bmi  is {bmi},you are Obese"
+    else:
+        return f" Your bmi  is {bmi},you are Clinically Obese"
+
 
 height = float(input("Enter your height: "))
 weight = float(input("Enter your weight: "))
 
-# The body mass index is the result from the division of weight by height squared
-BMI = round(weight / (height**2))
-
-
-if BMI < 18.5:
-    print(f" Your bmi  is {BMI},you are under weight")
-elif 18.5 < BMI < 25:
-    print(f" Your bmi  is {BMI},your weight is Normal")
-elif 25 < BMI < 30:
-    print(f" Your bmi  is {BMI},you are Overweight")
-elif 30 < BMI < 35:
-    print(f" Your bmi  is {BMI},you are Obese")
-else:
-    print(f" Your bmi  is {BMI},you are Clinically Obese")
+james = bmi_calc(height, weight)
